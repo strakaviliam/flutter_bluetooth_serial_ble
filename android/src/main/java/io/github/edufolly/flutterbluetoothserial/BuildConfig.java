0a003d93011c241f3b57a12d7c4b10ac448d3510
Viliam Straka <straka.viliam@gmail.com>
2025-01-25 15:21:55 +0100

    add missing file

---
+++ android/src/main/java/io/github/edufolly/flutterbluetoothserial/BuildConfig.java
@@ -0,0 +1,15 @@
+package io.github.edufolly.flutterbluetoothserial;
+
+public final class BuildConfig {
+    // Typically generated fields:
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+    public static final String LIBRARY_PACKAGE_NAME = "io.github.edufolly.flutterbluetoothserial";
+    public static final String BUILD_TYPE = "debug";
+    public static final String FLAVOR = "";
+    public static final int VERSION_CODE = 1;
+    public static final String VERSION_NAME = "1.0";
+
+    private BuildConfig() {
+        // Prevent instantiation
+    }
+}
